13af2af181d76360de24264349bd675b683fb359
neroices <git@slce.moe>
2024-05-09 22:50:35 +0700

    bahamut: Drop lineage-sdk
    Signed-off-by: neroices <git@slce.moe>

--- device.mk
+++ device.mk
@@ -42,9 +42,6 @@ PRODUCT_PACKAGES += \
     SonyBahamutFrameworksRes \
     SonyBahamutSystemUIRes
 
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
